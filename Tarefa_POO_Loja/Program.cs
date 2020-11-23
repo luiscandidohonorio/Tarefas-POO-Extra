5c95879752f8bad629c2445e7df6659c87aef879
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-11-23 16:12:15 -0300

    Update Program.cs

--- Tarefa_POO_Loja/Program.cs
+++ Tarefa_POO_Loja/Program.cs
@@ -1,15 +1,54 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using Course.Entities;
 
-namespace Tarefa_POO_Loja
+namespace TarefaPOOFinal_Loja
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Product> list = new List<Product>();
+            //Entre com os numeros dos produtos
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                //Dados do Produto
+                Console.WriteLine("Product #" + i + " data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+                else if (type == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+
+            Console.WriteLine();
+            //Dados de Saída
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
         }
     }
 }
