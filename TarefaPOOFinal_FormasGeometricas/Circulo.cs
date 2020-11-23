61462e58a69ecda7476e907ef66e0d92920c718e
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-11-23 12:05:49 -0300

    Add files via upload

---
+++ TarefaPOOFinal_FormasGeometricas/Circulo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CalcAreaPOO
+{
+    public class Circulo : Forma
+    {
+        public double Raio { get; set; }
+        public override double CalcularArea => Math.Pow(this.Raio, 2) * Math.PI;
+    }
+}
